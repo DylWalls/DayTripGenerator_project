8094f3b171934760946e9f24676670a33ec86d18
Dylan Walls <dylanwalls410@gmail.com>
2021-06-22 16:06:06 -0600

    Updated Function codes to simpilier coding with updates.

--- index.js
+++ index.js
@@ -1,103 +1,34 @@
-function randomLocation(str){
-  let num = (Math.floor(Math.random() * 5))
-    for(let i=0; i <= num; i++){
-        if (num == 0){
-            console.log("Rio")           
-        }
-        else if (num == 1){
-            console.log("Brazil")           
-        }
-        else if (num == 2){
-            console.log("Hungary")           
-        }
-        else if (num == 3){
-            console.log("Poland")     
-        }
-        else if (num == 4){
-            console.log("Turkey")           
-        }
-        else{}
-        return true;
-    }
-
+function randomLocation(array){
+  let num = Math.floor(Math.random() * array.length)
+  return array[num]
 }
 
 
+function randomBistro(array){
+    let num = (Math.floor(Math.random() * array.length))
+      return array[num]
+  }
 
-function randomBistro(str){
-    let num = (Math.floor(Math.random() * 5))
-      for(let i=0; i <= num; i++){
-          if (num == 0){
-              console.log("Hamburger")           
-          }
-          else if (num == 1){
-              console.log("Spaghetti")           
-          }
-          else if (num == 2){
-              console.log("Chicken")           
-          }
-          else if (num == 3){
-              console.log("Ribs")     
-          }
-          else if (num == 4){
-              console.log("Porkchops")           
-          }
-          else{}
-          return true;
-      }
-  
-  }
-  function randomTransport(str){
+  function randomTransport(array){
     let num = (Math.floor(Math.random() * 4))
-      for(let i=0; i <= num; i++){
-          if (num == 0){
-              console.log("Airplane")           
-          }
-          else if (num == 1){
-              console.log("Car")           
-          }
-          else if (num == 2){
-              console.log("Motorcycle")           
-          }
-          else if (num == 3){
-              console.log("Bicycle")     
-          }
-          else{}
-          return true;
-      }
-  
+     return array[num]
   }
-  function randomEntertain(str){
+
+  function randomEntertain(array){
     let num = (Math.floor(Math.random() * 5))
-      for(let i=0; i <= num; i++){
-          if (num == 0){
-              console.log("TV")           
-          }
-          else if (num == 1){
-              console.log("Phone")           
-          }
-          else if (num == 2){
-              console.log("Xbox")           
-          }
-          else if (num == 3){
-              console.log("Playstation")     
-          }
-          else if (num == 4){
-              console.log("Laptop")           
-          }
-          else{}
-          return true;
-      }
-  
+     return array[num]
   }
   
-  let placeToSee = "Rio, Brazil, Hungary, Poland, Turkey";
+  let placeToSee = ['Rio', 'Brazil', 'Hungary', 'Poland', 'Turkey']
   let randomPlace = randomLocation(placeToSee);
-  let placeToEat = "Hamburger, Spaghetti, Chicken, Ribs, Porkchops";
-  let randomfood = randomBistro(placeToEat);
-  let howToTransport = "Airplane, Car, Motorcycle, Bicycle";
+  
+  let placeToEat = ['hamburger', 'spaghetti', 'chicken', 'ribs', 'porkchops'];
+  let randomFood = randomBistro(placeToEat);
+  
+  let howToTransport = ['airplane', 'car', 'motorcycle', 'bicycle'];
   let randomTransp = randomTransport(howToTransport);
-  let wayToEntertain = "Tv, Phone, Xbox, Playstation, Laptop";
+  
+  let wayToEntertain = ['tv','phone', 'xbox', 'playstation', 'laptop'];
   let entertainment = randomEntertain(wayToEntertain);
-  let dayTrip = ("You are going to ", +randomLocation, " while eating ", +randomBistro, " in a ", +randomTransp, ", you are also watching ", +randomEntertain);
-  console.log(dayTrip);
+
+console.log("Your random destination is " + randomPlace + ". What you are going to have for dinner is " + randomFood + ", while riding a "+randomTransp + " also watching your "+entertainment + ".");
