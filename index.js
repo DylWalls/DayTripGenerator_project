f37984b6e71368f9f52af88dad683bb9351c8207
Dylan Walls <dylanwalls410@gmail.com>
2021-06-22 18:33:09 -0600

    Refactored randomized functions.

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
 
 let Yes = ("Y");
+let randomPlace;
+let randomFood;
+let randomTransp;
+let entertainment
+
+
 do {
-function randomLocation(array){
+function randomArrayitem(array){
   let num = Math.floor(Math.random() * array.length)
   return array[num]
 }
 
 
-function randomBistro(array){
-    let num = (Math.floor(Math.random() * array.length))
-      return array[num]
-  }
-
-  function randomTransport(array){
-    let num = (Math.floor(Math.random() * 4))
-     return array[num]
-  }
-
-  function randomEntertain(array){
-    let num = (Math.floor(Math.random() * 5))
-     return array[num]
- 
-  }
   let placeToSee = ['Rio', 'Brazil', 'Hungary', 'Poland', 'Turkey']
-  let randomPlace = randomLocation(placeToSee);
+   randomPlace = randomArrayitem(placeToSee);
   
   let placeToEat = ['an hamburger', 'spaghetti', 'chicken', 'ribs', 'porkchops'];
-  let randomFood = randomBistro(placeToEat);
+   randomFood = randomArrayitem(placeToEat);
   
   let howToTransport = ['airplane', 'car', 'motorcycle', 'bicycle'];
-  let randomTransp = randomTransport(howToTransport);
+   randomTransp = randomArrayitem(howToTransport);
   
   let wayToEntertain = ['tv','phone', 'xbox', 'playstation', 'laptop'];
-  let entertainment = randomEntertain(wayToEntertain);
+   entertainment = randomArrayitem(wayToEntertain);
   
   
   
 
-alert("Your random destination is " + randomPlace + ". What you are going to have for dinner is " + randomFood + ", while riding a "+randomTransp + " also watching a movie on your "+entertainment + ".");
+alert("Your random destination is " + randomPlace + ". What you are going to have for dinner is " + randomFood + ", while riding a "+randomTransp + " also watching a movie on your "+ entertainment + ".");
 userinput = prompt("Do you like the selection? Y/N");
 } while (userinput != Yes);
+console.log("Your random destination is " + randomPlace + ". What you are going to have for dinner is " + randomFood + ", while riding a "+randomTransp + " also watching a movie on your "+ entertainment + ".");
